5232eb7d3ee43d59214ee8a16d521d5e97bea058
Jaguer2001 <jaguer2001@gmail.com>
2022-02-22 01:14:35 -0600

    Actualizacion2

--- ProyectoDS_Reposteria/MainWindow.xaml.cs
+++ ProyectoDS_Reposteria/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace ProyectoDS_Reposteria
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
